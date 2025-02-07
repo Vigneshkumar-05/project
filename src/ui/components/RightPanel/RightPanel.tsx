b368697596ca5bc02580fda36d8ad6a1af3f3bab
iamswethaa <swethaelangovan733@gmail.com>
2025-02-07 10:55:28 +0530

    add functionality of right panel

--- src/ui/components/RightPanel/RightPanel.tsx
+++ src/ui/components/RightPanel/RightPanel.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Controls from "./Controls";
 import Faults from "./Faults";
 import Charts from "./Charts";
@@ -6,6 +7,7 @@ import Logs from "./Logs";
 
 const RightPanel: React.FunctionComponent = () => {
   const [activeTab, setActiveTab] = useState<string>("Controls");
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const renderContent = () => {
     switch (activeTab) {
@@ -23,26 +25,44 @@ const RightPanel: React.FunctionComponent = () => {
   };
 
   return (
-    <div className="top-3 flex flex-col bg-white px-1.5 py-1 h-full">
-      <div className="bg-gray-100 rounded-md flex p-1">
-        <nav className="flex p-1 justify-evenly">
-          {["Controls", "Faults", "Charts", "Logs"].map((tab) => (
+    <div className="flex h-full">
+      <div>
+        <div className="w-0.5 bg-gray-300 flex justify-center"> </div>
+          <div className="align-bottom items-baseline">
             <button
-              key={tab}
-              className={`px-5 py-1 rounded-md text-sm font-medium ${
-                activeTab === tab
-                  ? "bg-white text-black"
-                  : "text-black bg-gray-100"
-              }`}
-              onClick={() => setActiveTab(tab)}
+              className="flex items-center justify-center w-6 h-6 rounded-full border border-gray-100 bg-white shadow-sm"
+              onClick={() => setIsExpanded(!isExpanded)}
             >
-              {tab}
+              {isExpanded ? <AiOutlineLeft /> : <AiOutlineRight /> }
             </button>
-          ))}
-        </nav>
+          </div>
       </div>
-      {renderContent()}
+ 
+      {isExpanded && (
+        <div className="flex flex-col bg-white px-1.5 py-1">
+        <div className="bg-gray-100 rounded-md flex p-1">
+          <nav className="flex p-0.5 justify-evenly">
+            {["Controls", "Faults", "Charts", "Logs"].map((tab) => (
+              <button
+                key={tab}
+                className={`px-5 py-1 rounded-md text-sm font-medium ${
+                  activeTab === tab
+                    ? "bg-white text-black"
+                    : "text-black bg-gray-100"
+                }`}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </nav>
+        </div>
+        {renderContent()}
+      </div>
+      )}
     </div>
+
+
   );
 };
 
